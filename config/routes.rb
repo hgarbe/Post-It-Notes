0d51f32e6359cddfbc255962de7e48e6413f850d
hgarbe <h.garbe@live.com>
2019-02-19 09:06:21 -0700

    first

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'static_notes/home'
+  # get 'static_notes/about'
+  root 'static_notes#home'
+
+  #custome route
+  get "/about", to: "static_notes#about"
+
+ resources :notes 
+  # For details on the DSL available withinthis file, see http://guides.rubyonrails.org/routing.html
 end
